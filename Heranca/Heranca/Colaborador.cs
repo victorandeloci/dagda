2dab6ed4ea2437c023a2afb40a5922d15723062f
= <=>
2020-05-13 22:42:47 -0300

    some new classes

---
+++ Heranca/Heranca/Colaborador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Colaborador
+{
+    protected string nome;
+    protected string cpf;
+    protected float salario;
+
+    public string Nome { get => nome; set => nome = value; }
+    public string Cpf { get => cpf; set => cpf = value; }
+    public float Salario { get => salario; set => salario = value; }
+
+    public Colaborador() { }
+
+    public Colaborador(string nome, string cpf, float salario)
+    {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+
+    public void CalcularPLR()
+    {
+        Console.WriteLine("PLR: " + this.Salario * 0.1f);
+    }
+}
---
+++ Heranca/Heranca/Gerente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Gerente : Colaborador
+{
+    private string setor;
+
+    public string Setor { get => setor; set => setor = value; }
+
+    public Gerente() { }
+
+    public Gerente(string nome, string cpf, float salario, string setor)
+    {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+        this.setor = setor;
+    }
+
+    public virtual void CalcularPLR()
+    {
+        Console.WriteLine("PLR: " + this.Salario * 0.2f);
+    }
+
+}
